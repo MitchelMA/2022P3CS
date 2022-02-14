0e243412e2e08ae0eaeb2c6948a10cdb908afee3
MitchelMA <32548@ma-web.nl>
2022-02-14 10:59:42 +0100

    classes toegevoegd

---
+++ Gold/Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RekenMachine.Add(2, 3));
+            Console.WriteLine(RekenMachine.Div(12, 6));
+            Console.WriteLine(RekenMachine.Mult(2, 5));
+        }
+    }
+
+    static class RekenMachine
+    {
+        public static double Add(double inp1, double inp2)
+        {
+            return inp1 + inp2;
+        }
+        public static double Div(double inp1, double inp2)
+        {
+            return inp1 / inp2;
+        }
+        public static double Mult(double inp1, double inp2)
+        {
+            return inp1 * inp2;
+        }
+    }
+}
