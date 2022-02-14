69473c1ff199ca8919a92b143f02774deb6a82f0
MitchelMA <32548@ma-web.nl>
2022-02-14 11:53:55 +0100

    scopes toegevoegd

---
+++ Gold/Scopes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Scopes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void testFunction()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i == 5)
+                    {
+                        Console.WriteLine("i is 5");
+                    }
+                }
+            }
+            testFunction();
+        }
+    }
+}
