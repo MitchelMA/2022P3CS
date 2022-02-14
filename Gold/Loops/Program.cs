17b42fa2800bf4e7ab840ab0a9ec511134d3bf89
MitchelMA <32548@ma-web.nl>
2022-02-14 10:53:48 +0100

    Loops toegevoegd

---
+++ Gold/Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 2, 4, 6, 8, 10 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr.GetValue(i));
+            }
+        }
+    }
+}
