512b46a595749eee1d8aa1c30bd37ac451b83fea
MitchelMA <32548@ma-web.nl>
2022-02-14 11:38:57 +0100

    switch toegevoegd

---
+++ Gold/Switch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void loop()
+            {
+                Console.WriteLine("Geef een getal (0 of 1): ");
+                string inp = Console.ReadLine();
+
+                int parsed;
+                try
+                {
+                    parsed = int.Parse(inp); // correction voor getallen
+                    switch (parsed)
+                    {
+                        case 0:
+                            Console.WriteLine("Hallo, dit is case 0");
+                            break;
+                        case 1:
+                            Console.WriteLine("Hallo, dit is case 1");
+                            break;
+                        default:
+                            Console.WriteLine("Dit is de default case");
+                            break;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine("Dit is geen geldige invoer, voer opnieuw een getal in");
+                    loop();
+                }
+
+            }
+            loop();
+        }
+
+    }
+}
