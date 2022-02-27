2b0928f1cf417e15cddcab950491870c87e8c6fb
MitchelMA <32548@ma-web.nl>
2022-02-27 23:43:54 +0100

    Linq met queries toegevoegd

---
+++ Gold/Linq/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+// use the generic collection
+using System.Collections.Generic;
+// use the linq namespace
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create enumerable integer array
+            int[] myArray = { 10, 40, 33, 80, 91, 100, 50, 81, 67, 83 };
+
+            // define a query expression
+            IEnumerable<int> query =
+                from integer in myArray
+                where integer > 70
+                select integer;
+
+            // execute the query
+            foreach (int i in query)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // string length
+            string[] array = { "Dit is kort", "Dit is veel langer dan de vorige", "Bij deze is het de bedoeling dat het nog veel langer zal zijn", "kortst" };
+            IEnumerable<string> queryS =
+                from S in array
+                where S.Length > 30
+                select S;
+            IEnumerable<string> queryS2 =
+                from S in array
+                where S.Length < 30
+                select S;
+
+            foreach (string s in queryS)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            foreach (string s in queryS2)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
